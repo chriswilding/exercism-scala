ed0ad62e03ffea7fc85d89be4762454d0a923030
Chris Wilding <hello@chriswilding.co.uk>
2018-05-27 16:21:56 +0100

    Add space age

---
+++ space-age/src/main/scala/SpaceAge.scala
@@ -0,0 +1,27 @@
+object SpaceAge {
+  val SecondsInYearOnEarth = 31557600
+
+  def onEarth(seconds: Double, orbitalPeriod: Double = 1): Double =
+    BigDecimal(seconds / (orbitalPeriod * SecondsInYearOnEarth)).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+
+  def onMercury(seconds: Double): Double =
+    onEarth(seconds, 0.2408467)
+
+  def onVenus(seconds: Double): Double =
+    onEarth(seconds, 0.61519726)
+
+  def onMars(seconds: Double): Double =
+    onEarth(seconds, 1.8808158)
+
+  def onJupiter(seconds: Double): Double =
+    onEarth(seconds, 11.862615)
+
+  def onSaturn(seconds: Double): Double =
+    onEarth(seconds, 29.447498)
+
+  def onUranus(seconds: Double): Double =
+    onEarth(seconds, 84.016846)
+
+  def onNeptune(seconds: Double): Double =
+    onEarth(seconds, 164.79132)
+}
