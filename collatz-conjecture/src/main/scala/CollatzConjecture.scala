45848779df2fb42cb3c13e609cfae7bb29c78fe4
Chris Wilding <hello@chriswilding.co.uk>
2018-05-28 16:56:34 +0100

    Update collatz conjecture

--- collatz-conjecture/src/main/scala/CollatzConjecture.scala
+++ collatz-conjecture/src/main/scala/CollatzConjecture.scala
@@ -1,13 +1,10 @@
 object CollatzConjecture {
-  def steps(i: Int): Option[Int] = {
-    if (i < 1) None
-    else Some(calculate(i))
-  }
-
-  private def calculate(i: Int, step: Int = 0): Int =
-    i match {
-      case i if i == 1 => step
-      case i if i % 2 == 0 => calculate(i / 2, step + 1)
-      case i => calculate(i * 3 + 1, step + 1)
+  def steps(n: Int, step: Int = 0): Option[Int] = {
+    n match {
+      case n if n < 1 => None
+      case 1 => Some(step)
+      case even if n % 2 == 0 => steps(even / 2, step + 1)
+      case odd => steps(odd * 3 + 1, step + 1)
     }
+  }
 }
