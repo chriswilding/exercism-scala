f0e6eebb1832e3d5e3b8cf46a3c870d2d27d945f
Chris Wilding <hello@chriswilding.co.uk>
2018-06-01 21:08:33 +0100

    Update pangram

--- pangram/src/main/scala/Pangrams.scala
+++ pangram/src/main/scala/Pangrams.scala
@@ -1,8 +1,6 @@
 object Pangrams {
   val alphabet = 'a' to 'z'
 
-  def isPangram(input: String): Boolean = {
-    val string = input.toLowerCase
-    string.length > 25 && alphabet.forall(char => string.contains(char))
-  }
+  def isPangram(input: String): Boolean =
+    input.toLowerCase.filter(_.isLetter).toSet.size == 26
 }
