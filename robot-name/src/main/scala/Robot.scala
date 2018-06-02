2d339fea6426b151fdb6f5ba5a3fe77991d85184
Chris Wilding <hello@chriswilding.co.uk>
2018-06-02 15:03:55 +0100

    Update robot name
    * Use Iterator[String] for names

--- robot-name/src/main/scala/Robot.scala
+++ robot-name/src/main/scala/Robot.scala
@@ -1,18 +1,20 @@
+import scala.util.Random
+
 class Robot(var name: String = Robot.getName) {
   def reset() =
     this.name = Robot.getName
 }
 
 object Robot {
-  var names = for {
-    a <- 'A' to 'Z'
-    b <- 'A' to 'Z'
-    n <- 0 to 999
-  } yield f"$a$b$n%03d"
+  val names: Iterator[String] = Random.shuffle(
+    for {
+      a <- 'A' to 'Z'
+      b <- 'A' to 'Z'
+      n <- 0 to 999
+    } yield f"$a$b$n%03d"
+  ).toIterator
 
   def getName(): String = {
-    val name = names.head
-    names = names.drop(1)
-    name
+    names.next
   }
 }
