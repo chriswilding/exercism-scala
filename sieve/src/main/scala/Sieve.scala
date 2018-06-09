db37ecdbfcf19f4ffdb8a00bd3e2be7ea655305b
Chris Wilding <hello@chriswilding.co.uk>
2018-06-09 12:50:33 +0100

    Update sieve

--- sieve/src/main/scala/Sieve.scala
+++ sieve/src/main/scala/Sieve.scala
@@ -3,13 +3,9 @@ object Sieve {
     if (n == 1) List.empty
     else {
       val candidates = 3.to(n, 2).toList
-      2 :: sieve(candidates, List.empty).reverse
-    }
-
-  @annotation.tailrec
-  private def sieve(candidates: List[Int], primes: List[Int]): List[Int] =
-    candidates match {
-      case Nil => primes
-      case head :: tail => sieve(tail.filter(n => n % head != 0), head :: primes)
+      val primes = candidates.foldLeft(candidates) {
+        (primes, current) => primes.filter(i => i == current || i % current != 0)
+      }
+      2 :: primes
     }
 }
