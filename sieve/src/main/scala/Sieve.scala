51911d3669745cce121c5da76f2bc784bbb81153
Chris Wilding <hello@chriswilding.co.uk>
2018-06-09 13:33:44 +0100

    Update sieve

--- sieve/src/main/scala/Sieve.scala
+++ sieve/src/main/scala/Sieve.scala
@@ -2,8 +2,9 @@ object Sieve {
   def primes(n: Int): List[Int] =
     if (n == 1) List.empty
     else {
-      val candidates = 3.to(n, 2).toList
-      val primes = candidates.foldLeft(candidates) {
+      val candidates = 3.to(math.sqrt(n).toInt, 2).toList
+      val numbers = 3.to(n, 2).toList
+      val primes = candidates.foldLeft(numbers) {
         (primes, current) => primes.filter(i => i == current || i % current != 0)
       }
       2 :: primes
