685f71f208256e4a6ac6b4ab2f6ad8150f0fbbf8
Chris Wilding <hello@chriswilding.co.uk>
2018-06-10 11:57:48 +0100

    Update word count

--- word-count/src/main/scala/WordCount.scala
+++ word-count/src/main/scala/WordCount.scala
@@ -1,10 +1,9 @@
 case class WordCount(phrase: String) {
-  def countWords(): Map[String, Int] = {
+  def countWords(): Map[String, Int] =
     phrase
       .toLowerCase()
       .split("""'?[^\d|\w|']'?""")
       .filter(_.nonEmpty)
       .groupBy(identity)
       .mapValues(_.size)
-  }
 }
