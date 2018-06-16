0a7d92860e97d12a413a8a30c380246bd9c7a609
Chris Wilding <hello@chriswilding.co.uk>
2018-06-16 15:38:23 +0100

    Update allergies

--- allergies/src/main/scala/Allergies.scala
+++ allergies/src/main/scala/Allergies.scala
@@ -1,6 +1,6 @@
 object Allergies {
   def allergicTo(allergen: Allergen.Value, score: Int): Boolean =
-    (allergen.id & score) > 0
+    ((1 << allergen.id) & score) > 0
 
   def list(score: Int): List[Allergen.Value] =
     Allergen
@@ -10,12 +10,5 @@ object Allergies {
 }
 
 object Allergen extends Enumeration {
-  val Eggs = Value(1)
-  val Peanuts = Value(2)
-  val Shellfish = Value(4)
-  val Strawberries = Value(8)
-  val Tomatoes = Value(16)
-  val Chocolate = Value(32)
-  val Pollen = Value(64)
-  val Cats = Value(128)
+  val Eggs, Peanuts, Shellfish, Strawberries, Tomatoes, Chocolate, Pollen, Cats = Value
 }
