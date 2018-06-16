82f2a20ca97f864f744ff5e787f2f48a13152024
Chris Wilding <hello@chriswilding.co.uk>
2018-06-16 15:12:45 +0100

    Add prime factors

---
+++ prime-factors/src/main/scala/PrimeFactors.scala
@@ -0,0 +1,15 @@
+object PrimeFactors {
+  def factors(n: Long): List[Long] = {
+    if (n == 1) List.empty
+    else {
+      getFactors(n, 2, List.empty[Long]).reverse
+    }
+  }
+
+  @annotation.tailrec
+  private def getFactors(n: Long, factor: Long, factors: List[Long]): List[Long] = {
+    if (n == factor) factor :: factors
+    else if (n % factor == 0) getFactors(n / factor, 2, factor :: factors)
+    else getFactors(n, factor + 1, factors)
+  }
+}
