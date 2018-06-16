7d4386b366bf8573778a2efc5a2e131298b3dbf1
Chris Wilding <hello@chriswilding.co.uk>
2018-06-16 15:00:21 +0100

    Update meetup

--- meetup/src/main/scala/Meetup.scala
+++ meetup/src/main/scala/Meetup.scala
@@ -1,22 +1,26 @@
-import java.time.{DayOfWeek, LocalDate}
+import java.time.{DayOfWeek, LocalDate, YearMonth}
 
 import Schedule.Schedule
 
 case class Meetup(month: Int, year: Int) {
 
   def day(dayOfWeek: Int, schedule: Schedule): LocalDate = {
-    val lengthOfMonth = LocalDate.of(year, month, 1).lengthOfMonth()
-    val daysInMonth = (1 to lengthOfMonth).map(LocalDate.of(year, month, _))
-
-    schedule match {
-      case Schedule.Teenth => daysInMonth.find(date =>
-        date.getDayOfMonth > 12 && date.getDayOfMonth < 20 && date.getDayOfWeek.getValue == dayOfWeek).get
-      case Schedule.First => daysInMonth.find(date => date.getDayOfWeek.getValue == dayOfWeek).get
-      case Schedule.Second => daysInMonth.filter(date => date.getDayOfWeek.getValue == dayOfWeek).slice(1, 2)(0)
-      case Schedule.Third => daysInMonth.filter(date => date.getDayOfWeek.getValue == dayOfWeek).slice(2, 3)(0)
-      case Schedule.Fourth => daysInMonth.filter(date => date.getDayOfWeek.getValue == dayOfWeek).slice(3, 4)(0)
-      case Schedule.Last => daysInMonth.reverse.find(date => date.getDayOfWeek.getValue == dayOfWeek).get
+    val range = schedule match {
+      case Schedule.Teenth => Stream.range(13, 20)
+      case Schedule.First => Stream.range(1, 8)
+      case Schedule.Second => Stream.range(8, 15)
+      case Schedule.Third => Stream.range(15, 22)
+      case Schedule.Fourth => Stream.range(22, 29)
+      case Schedule.Last => {
+        val lengthOfMonth = YearMonth.of(year, month).lengthOfMonth()
+        Stream.range(lengthOfMonth, lengthOfMonth - 7, -1)
+      }
     }
+
+    range
+      .map(LocalDate.of(year, month, _))
+      .find(_.getDayOfWeek.getValue == dayOfWeek)
+      .get
   }
 }
 
