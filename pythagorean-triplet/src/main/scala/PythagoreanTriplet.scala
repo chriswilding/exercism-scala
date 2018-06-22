e9ccbd57f80d7b01f297ad29ca239ecde667df6b
Chris Wilding <hello@chriswilding.co.uk>
2018-06-22 23:22:19 +0100

    Update pythagorean triplet

--- pythagorean-triplet/src/main/scala/PythagoreanTriplet.scala
+++ pythagorean-triplet/src/main/scala/PythagoreanTriplet.scala
@@ -1,17 +1,14 @@
 object PythagoreanTriplet {
   def isPythagorean(triplet: (Int, Int, Int)): Boolean = {
     val a :: b :: c :: Nil = List(triplet._1, triplet._2, triplet._3).sorted
-    math.pow(a, 2) + math.pow(b, 2) == math.pow(c, 2)
+    a * a + b * b == c * c
   }
 
-  def pythagoreanTriplets(start: Int, end: Int): Seq[(Int, Int, Int)] = {
-    val triplets = for {
-      a <- start to end
-      b <- (a + 1) to end
-      c <- (b + 1) to end
-      if isPythagorean((a, b, c))
-    } yield (a, b, c)
-
-    triplets.toSet.toSeq
-  }
+  def pythagoreanTriplets(start: Int, end: Int): Seq[(Int, Int, Int)] =
+    (start to end)
+        .toList
+        .combinations(3)
+        .map { case a :: b :: c :: Nil => (a, b, c) }
+        .filter(isPythagorean)
+        .toSeq
 }
