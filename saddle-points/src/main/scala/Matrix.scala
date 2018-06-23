6b7fcfdcbbeca20f54abb89d4080cc18b3034f47
Chris Wilding <hello@chriswilding.co.uk>
2018-06-23 11:40:26 +0100

    Update saddle points

--- saddle-points/src/main/scala/Matrix.scala
+++ saddle-points/src/main/scala/Matrix.scala
@@ -1,34 +1,12 @@
-case class Matrix(matrix: List[List[Int]]) {
+case class Matrix(rows: List[List[Int]]) {
+  val columns = rows.transpose
+
   def saddlePoints(): Set[(Int, Int)] = {
-    if (matrix.forall(row => row.isEmpty)) Set.empty
-    else getSaddlePoints()
-  }
-
-  private def getSaddlePoints(): Set[(Int, Int)] = {
-    val largest = matrix.flatMap(row => {
-      val max = row.max
-      row.zipWithIndex.filter(
-        Function.tupled((value, _) => value == max)
-      )
-    })
-
-    largest
-      .foldLeft(Set.empty[(Int, Int)])(
-        (acc, rowWithIndex) => {
-          val column = matrix.map(column => column(rowWithIndex._2))
-          val min = column.min
-          val smallest = column.zipWithIndex.filter(
-            Function.tupled((value, _) => value == min)
-          )
-
-          smallest.foldLeft(acc)(
-            (acc, columnWithIndex) => {
-              if (columnWithIndex._1 == rowWithIndex._1) {
-                acc + ((columnWithIndex._2, rowWithIndex._2))
-              } else acc
-            }
-          )
-        }
-      )
+    val saddlePoints = for (
+      x <- rows.indices;
+      y <- columns.indices
+      if rows(x).max == columns(y).min
+    ) yield (x, y)
+    saddlePoints.toSet
   }
 }
