6d255923fce0146b70ecaa7cb2526ceb53de417b
Chris Wilding <hello@chriswilding.co.uk>
2018-06-24 14:40:59 +0100

    Update run length encoding

--- run-length-encoding/src/main/scala/RunLengthEncoding.scala
+++ run-length-encoding/src/main/scala/RunLengthEncoding.scala
@@ -3,37 +3,51 @@ object RunLengthEncoding {
   private val decodeRegex = """([ A-z])\1*|\d+""".r
 
   def encode(input: String): String = {
-    encodeRegex
-      .findAllIn(input)
-      .foldLeft(new StringBuilder)(
-        (sb, run) => {
-          if (run.length == 1) sb.append(run)
-          else {
-            sb.append(run.length)
-            sb.append(run.head)
-          }
-        }
-      )
-      .toString
+    val matches = encodeRegex.findAllIn(input)
+
+    val stringBuilder = new StringBuilder
+
+    matches.foreach {
+      case run if run.length == 1 => stringBuilder.append(run)
+      case run => {
+        stringBuilder.append(run.length)
+        stringBuilder.append(run.head)
+      }
+    }
+
+    stringBuilder.toString
   }
 
   def decode(input: String): String = {
-    val matches = decodeRegex.findAllIn(input)
+    val matches = decodeRegex.findAllIn(input).toList
+    val grouped = group(matches).reverse
 
-    val sb = new StringBuilder
+    val stringBuilder = new StringBuilder
 
-    var count = 1
-
-    while (matches.hasNext) {
-      val countOrChar = matches.next
-      if (countOrChar.head.isDigit) count = countOrChar.toInt
-      else {
+    grouped.foreach {
+      case (count, char) if count == 1 => stringBuilder.append(char)
+      case (count, char) => {
         for (_ <- 0 until count) {
-          sb.append(countOrChar)
+          stringBuilder.append(char)
         }
-        count = 1
       }
     }
-    sb.toString()
+
+    stringBuilder.toString
+  }
+
+  @annotation.tailrec
+  def group(input: List[String], output: List[(Int, Char)] = List.empty): List[(Int, Char)] = {
+    (input, output) match {
+      case (Nil, out) => out
+      case (countOrChar :: tail, out) => {
+        if (countOrChar.head.isDigit) {
+          val count = countOrChar.toInt
+          group(tail.tail, (count, tail.head.head) :: out)
+        } else {
+          group(tail, (1, countOrChar.head) :: out)
+        }
+      }
+    }
   }
 }
