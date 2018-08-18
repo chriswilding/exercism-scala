ccfab35afeb9e51fed2b219eb0b82860c2525b4b
Chris Wilding <hello@chriswilding.co.uk>
2018-08-18 22:07:36 +0100

    Update grains

--- grains/src/main/scala/Grains.scala
+++ grains/src/main/scala/Grains.scala
@@ -1,10 +1,10 @@
 object Grains {
-  private val two = BigInt(2)
+  private val one = BigInt(1)
 
   def square(n: Int): Option[BigInt] =
-    if (n > 0 && n < 65) Some(two.pow(n - 1))
+    if (n > 0 && n < 65) Some(one << n - 1)
     else None
 
   def total(): BigInt =
-    (1 to 64).flatMap(n => square(n)).sum
+    (one << 64) - 1
 }
