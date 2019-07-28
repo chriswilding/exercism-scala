e3d196ce38f7be7600f86b09ddb37f43765a167b
Chris Wilding <hello@chriswilding.co.uk>
2019-07-28 21:57:48 +0100

    Update luhn

--- luhn/src/main/scala/Luhn.scala
+++ luhn/src/main/scala/Luhn.scala
@@ -5,12 +5,10 @@ object Luhn {
         if (input.length < 2) false
         else if (input.exists(!_.isDigit)) false
         else {
-            val sum = input.reverse.zipWithIndex.map { charIndex =>
-                val number = charIndex._1.asDigit
-                val index = charIndex._2
-                if (index % 2 == 0) number
-                else {
-                    val double = number * 2
+            val sum = input.reverse.zipWithIndex.map {
+                case (char, index) if index % 2 == 0 => char.asDigit
+                case (char, _) => {
+                    val double = char.asDigit * 2
                     if (double > 9) double - 9 else double
                 }
             }.sum
